4adbe9a6ae0455f1935f74745894c6a7516d7b7a
vagrant <vagrant@playground>
2025-07-08 06:53:45 +0000

    updating config for shopify

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -11,9 +11,10 @@ class ShopifyController extends Controller
     public function index()
     {
         $response = Http::withHeaders([
-            'X-Shopify-Access-Token' => env('SHOPIFY_ACCESS_TOKEN'),
+            'X-Shopify-Access-Token' => config('shopify.access_token'),
             'Content-Type' => 'application/json',
-        ])->get('https://' . env('SHOPIFY_STORE_URL') . '/admin/api/' . env('SHOPIFY_API_VERSION') . '/products.json');
+        ])->get('https://' . config('shopify.store_url') . '/admin/api/' . config('shopify.api_version')
+ . '/products.json');
 
         $products = $response->json()['products'];
 
---
+++ config/shopify.php
@@ -0,0 +1,5 @@
+<?php return [
+    'access_token' => env('SHOPIFY_ACCESS_TOKEN'),
+    'store_url' => env('SHOPIFY_STORE_URL'),
+    'api_version' => env('SHOPIFY_API_VERSION'),
+];
